fa4c9d87729feeaa45a7769488a4c256bdb59b13
eav93 <eav@vh5.ru>
2026-03-23 23:04:04 +0300

    Parse device model as PLERY-R626 from version string

--- rootfs/www-comfast/common/index.js
+++ rootfs/www-comfast/common/index.js
@@ -235,9 +235,9 @@ define(function (require, exports) {
 
         d('#mac').html(firmware_info.macaddr.toUpperCase());
         if (firmware_info) {
-            deviceReg = new RegExp(/(.+)-V(.+)/);
+            deviceReg = new RegExp(/^CF-(.+?)-eav/);
             var match = deviceReg.exec(firmware_info.version);
-            deviceName = match ? match[1].split('-')[1] : 'Plery R626';
+            deviceName = match ? match[1] : ((/(.+)-V(.+)/).exec(firmware_info.version) || ['','',firmware_info.version])[1].split('-')[1];
             d("#deviceinfo").text(deviceName);
             d("#version").text(firmware_info.version);
         }
