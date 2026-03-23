14d003348ffceb0e34f832d5a78fac6f4ac61838
eav93 <eav@vh5.ru>
2026-03-23 22:29:42 +0300

    Fix OTA support: version must start with CF- for webmgnt check

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ FWTOOL_DIR="fwtool"
 FWTOOL="$FWTOOL_DIR/fwtool"
 FWTOOL_REPO="https://git.openwrt.org/project/fwtool.git"
 FWTOOL_COMMIT="04cd252e4e9394ffacd51f56f1f124abc534f715"
-BASE_VERSION="Plery-R626-eav93"
+BASE_VERSION="CF-PLERY-R626-eav93"
 
 # ---- Colors ----
 RED='\033[0;31m'
